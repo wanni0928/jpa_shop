dccf73f5c98fba9f7163fdac473f00eb622a517f
wanni0928 <chlgudwnd123@gmail.com>
2020-09-04 09:50:05 +0900

    entity, repository, service test

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void joinTest() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("choi");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void checkDuplicateTest() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("choi");
+
+        Member member2 = new Member();
+        member2.setName("choi");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2); //Exception!!
+
+        //then
+        fail("에외가 발생해야 한다.");
+    }
+}
